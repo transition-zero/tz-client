672811e154916d5bdb1e3bf17a1ba775dbbccf3b
Lucas <lucas.kruitwagen@gmail.com>
2023-02-03 13:03:29 +0000

    auth login and test

---
+++ transitionzero/client/auth.py
@@ -0,0 +1,82 @@
+import json
+import os
+import sys
+import time
+
+import requests
+from loguru import logger
+
+logger.remove()
+logger.add(
+    sys.stdout, colorize=False, format="{time:YYYYMMDDHHmmss}|{level}| {message}"
+)
+
+
+AUTH0_CLIENT_ID = os.environ.get("AUTH0_CLIENT_ID", "bjzlyhLEClss31537pW3lu3Fkg97TzZ8")
+AUTH0_DOMAIN = os.environ.get("AUTH0_DOMAIN", "dev-feo-tz.eu.auth0.com")
+AUTH0_AUDIENCE = "https://auth.feo.transitionzero.org/"
+ALGORITHMS = ["RS256"]
+
+
+def login(config=None):
+    """
+    Runs the device authorization flow, writes a long-expiry JWT to a new hidden folder in the $HOME directory
+    """
+
+    token_dir = os.path.join(os.path.expanduser("~"), ".tz-feo")
+
+    if not os.path.exists(token_dir):
+        logger.info(f"Writing token directory {token_dir}")
+        os.makedirs(token_dir)
+
+    device_code_payload = {"client_id": AUTH0_CLIENT_ID, "scope": "openid profile"}
+
+    device_code_response = requests.post(
+        f"https://{AUTH0_DOMAIN}/oauth/device/code", data=device_code_payload
+    )
+
+    if device_code_response.status_code != 200:
+        logger.error("Error generating the device code")
+        raise ValueError("Error generating device code")
+
+    logger.info("Device code successful")
+    device_code_data = device_code_response.json()
+    print(
+        "1. In a browser navigate to: ", device_code_data["verification_uri_complete"]
+    )
+    print("2. Enter the following code: ", device_code_data["user_code"])
+
+    token_payload = {
+        "grant_type": "urn:ietf:params:oauth:grant-type:device_code",
+        "device_code": device_code_data["device_code"],
+        "client_id": AUTH0_CLIENT_ID,
+    }
+
+    authenticated = False
+    print("Checking for authentication", end="")
+    while not authenticated:
+        token_response = requests.post(
+            f"https://{AUTH0_DOMAIN}/oauth/token", data=token_payload
+        )
+
+        token_data = token_response.json()
+        if token_response.status_code == 200:
+            print("Authenticated!")
+            print("- Id Token: {}...".format(token_data["id_token"][:10]))
+            authenticated = True
+        elif token_data["error"] not in ("authorization_pending", "slow_down"):
+            print(token_data["error_description"])
+            raise ValueError("Error generating device code")
+        else:
+            print(".", end="")
+            time.sleep(device_code_data["interval"])
+
+    # success!
+    # TODO: use our new token to fetch our current_user
+    # display a friendly welcome... print ('Hello {name}!')
+
+    # write token data to file
+    json.dump(
+        token_data,
+        open(os.path.join(os.path.expanduser("~"), ".tz-feo", "token.json"), "w"),
+    )
---
+++ transitionzero/client/cli/auth.py
@@ -0,0 +1,50 @@
+import json
+import os
+import sys
+
+import click
+import requests
+from loguru import logger
+
+from transitionzero.client.auth import login as auth_login
+from transitionzero.client.cli.cli import root
+
+logger.remove()
+logger.add(
+    sys.stdout, colorize=False, format="{time:YYYYMMDDHHmmss}|{level}| {message}"
+)
+
+
+@root.group()
+@click.pass_obj
+def auth(config):
+    """Authentication operations"""
+
+
+@auth.command()
+@click.pass_obj
+def login(config):
+    """
+    Runs the device authorization flow, writes a long-expirey JWT to a new hidden folder in the $HOME directory
+    """
+
+    auth_login()
+
+
+@auth.command()
+@click.pass_obj
+def test(config):
+    URL = "https://power-legacy.feo.transitionzero.org"
+    token = json.load(
+        open(os.path.join(os.path.expanduser("~"), ".tz-feo", "token.json"))
+    )
+
+    headers = {"Authorization": "Bearer {}".format(token["access_token"])}
+
+    params = {"admin_0": "ID", "page": 3, "limit": 5}
+
+    r = requests.get(URL + "/units/", params=params, headers=headers)
+
+    print(r.status_code)
+    units = json.loads(r.text)["units"]
+    print(units)
