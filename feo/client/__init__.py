c42063260f634b74067db012c45fbdeb3edc5c7b
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2023-10-23 22:34:43 +0100

    chore: cleanup

--- feo/client/__init__.py
+++ feo/client/__init__.py
@@ -1,27 +1,11 @@
-from feo.client.asset import Asset, AssetCollection
-from feo.client.model import Model
+# from feo.client.asset import Asset, AssetCollection
+# from feo.client.model import Model
 from feo.client.node import Node
-from feo.client.record import (
-    Event,
-    Generation,
-    LandCover,
-    Price,
-    Record,
-    RecordCollection,
-    TechnologyParameter,
-)
 
-__all__ = [
-    "Model",
-    "Node",
-    "Asset",
-    "AssetCollection",
-    "Record",
-    "RecordCollection",
-    "TechnologyParameter",
-    "Generation",
-    "Price",
-    "Event",
-    "LandCover",
-]
+try:
+    import pydantic.v1 as pydantic  # type: ignore
+except ImportError:
+    import pydantic  # type: ignore[no-redef]
+
+__all__ = ["Model", "Node", "Asset", "AssetCollection"]
 __version__ = "0.0.1"
--- feo/client/asset.py
+++ feo/client/asset.py
@@ -2,10 +2,10 @@
 
 import pandas as pd
 
-from feo.client.base import Base
+from feo.client import schemas
 
 
-class Asset(Base):
+class Asset(schemas.Asset):
     def __init__(self, asset_id):
         pass
 
@@ -14,27 +14,10 @@ class Asset(Base):
         pass
 
 
-class AssetCollection(Base):
-    def __init__(self, node_id: str, sector: str):
-        self.node_id = node_id
-        self.sector = sector
-        self._assets = Asset.search(node_id=node_id, sector=sector)
-
-    def __get_item__(self, i):
-        return self._assets[i]
-
-    def to_dataframe(self):
-        return pd.DataFrame([a.json() for a in self._assets])
-
-    def json(self):
+class AssetCollection(pd.DataFrame):
+    @classmethod
+    def from_parent_node(cls, node_id: str):
         pass
 
-    def __repr__(self):
-        return f"AssetCollect(node_id={self.node_id},link_id={self.link_id},node_level={self.node_level},sector={self.sector})"
-
-    def __str__(self):
-        return f"AssetCollect(node_id={self.node_id},link_id={self.link_id},node_level={self.node_level},sector={self.sector}) with {len(self._assets)} assets"
-
-    @classmethod
-    def reduce_technologies(cls: str, node_id: str, sector: str):
-        Base().client.get("")
+    def next_page(self):
+        pass
