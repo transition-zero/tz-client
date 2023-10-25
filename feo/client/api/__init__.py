eafbc62bb053fc48db43168bf341168568524394
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2023-10-25 14:38:19 +0100

    feat: finish pairback and initial feature

--- feo/client/api/__init__.py
+++ feo/client/api/__init__.py
@@ -1,3 +1,28 @@
+"""
+    `feo` stands for the Future Energy Outlook, by TransitionZero.
+    The Future Energy Outlook
+    This package, `feo-client`, allows users to interact with the platform via both a low-level api wrapper,
+    and a set of higher-order class objects.
+
+    First authenticate via the command line
+    ```
+    feo auth login
+    ```
+
+    To import the feo api:
+    ```python
+    from feo.client import api
+    ```
+
+    Calls to underlying data tables can then be made using the api:
+    ```python
+    nodes = api.nodes.get(id="IDN")
+    ```
+    ```python
+    my_model = api.models.get("my-model")
+    ```
+"""
+
 from feo.client.api.aliases import AliasAPI
 
 aliases = AliasAPI()
