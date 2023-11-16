177e4740317e3fc6b35d35e10ad00c6944a01b5b
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2023-11-16 11:35:00 +0000

    fix: cleanup after logging merge

--- feo/client/core/logging.py
+++ feo/client/core/logging.py
@@ -1,9 +1,5 @@
 import logging
 import sys
 
-<<<<<<< HEAD
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-=======
 logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
->>>>>>> f82e829 (chore: merge upstream)
 logger = logging.getLogger(__name__)
