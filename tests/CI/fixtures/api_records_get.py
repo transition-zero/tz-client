68ddbc2aab9f58dcbfb9eee86ac7094e9c321415
Calvin Nesbitt <cf.nesbitt94@gmail.com>
2023-11-23 16:56:28 +0000

    Feat: added test scaffold

--- tests/CI/fixtures/api_records_get.py
+++ tests/CI/fixtures/api_records_get.py
@@ -4,8 +4,5 @@ import pytest
 @pytest.fixture
 def records_get_cases():
     return [
-        (
-            dict(alias="indonesia", node_type="admin_0"),
-            ["IDN"],
-        ),  # TODO: Get example records
+        (),  # TODO: Get example records
     ]
