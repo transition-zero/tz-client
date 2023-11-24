0ab4196ea5189cba5a15934dee5a4fce41ce401e
Joe O'Connor <joe@transitionzero.org>
2023-11-24 09:26:21 +0000

    refactor: :recycle: Implement search and get methods for runs API

--- tests/CI/test_run_api.py
+++ tests/CI/test_run_api.py
@@ -1,7 +1,44 @@
+from datetime import datetime
+
+import pytest
+
 from feo.client import api
 
+EXAMPLE_PARAMS = [
+    dict(
+        fullslug="feo-global-indonesia:coal-retirement:main",
+        includes=None,
+        start_datetime=None,
+        end_datetime=None,
+    ),
+    dict(
+        fullslug="feo-global-indonesia:coal-retirement:main",
+        includes=None,
+        start_datetime=datetime(2010, 1, 1),
+        end_datetime=None,
+    ),
+    dict(
+        fullslug="feo-global-indonesia:coal-retirement:main",
+        includes=None,
+        start_datetime=None,
+        end_datetime=datetime(2020, 1, 1),
+    ),
+    dict(
+        fullslug="feo-global-indonesia:coal-retirement:main",
+        includes=None,
+        start_datetime=datetime(2010, 1, 1),
+        end_datetime=datetime(2020, 1, 1),
+    ),
+]
 
-def test_run_api():
-    runs = api.runs.get()
+
+@pytest.mark.parametrize("run_params", EXAMPLE_PARAMS)
+def test_run_get(run_params):
+    run = api.runs.get(**run_params)
+    assert isinstance(run, api.schemas.Run)
+
+
+def test_run_search():
+    runs = api.runs.search()
     assert isinstance(runs, list)
     assert isinstance(runs[0], api.schemas.Run)
