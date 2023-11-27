bee9bc54d0d2fbab222377b2dd33cdcc6a744895
Joe O'Connor <joe@transitionzero.org>
2023-11-27 09:13:39 +0000

    test: add scenario test

--- tests/CI/test_scenario.py
+++ tests/CI/test_scenario.py
@@ -11,3 +11,20 @@ def test_scenario_search():
     scenarios = Scenario.search()
     assert isinstance(scenarios, list)
     assert isinstance(scenarios[0], Scenario)
+
+
+def test_search_pagination():
+    PAGE_LIMIT = 2
+    items1 = Scenario.search(limit=PAGE_LIMIT, page=0)
+    assert len(items1) == PAGE_LIMIT
+    items2 = Scenario.search(limit=PAGE_LIMIT, page=1)
+    assert len(items2) == PAGE_LIMIT
+
+    # assert that no items are returned when page number is too high
+    items_bad = Scenario.search(limit=PAGE_LIMIT, page=10000)
+    assert len(items_bad) == 0
+
+    ids1 = {item.id for item in items1}
+    ids2 = {item.id for item in items2}
+    # assert that items on different pages are all different
+    assert ids1.intersection(ids2) == set()
