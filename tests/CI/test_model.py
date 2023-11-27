b804a8cfb2bb7699b518acaccdeff6369414e484
Joe O'Connor <joe@transitionzero.org>
2023-11-27 09:09:01 +0000

    test: :test_tube: Add pagination tests to node model and run classes

--- tests/CI/test_model.py
+++ tests/CI/test_model.py
@@ -11,3 +11,20 @@ def test_model_search():
     models = Model.search()
     assert isinstance(models, list)
     assert isinstance(models[0], Model)
+
+
+def test_search_pagination():
+    PAGE_LIMIT = 1
+    items1 = Model.search(limit=PAGE_LIMIT, page=0)
+    assert len(items1) == PAGE_LIMIT
+    items2 = Model.search(limit=PAGE_LIMIT, page=1)
+    assert len(items2) == PAGE_LIMIT
+
+    # assert that no items are returned when page number is too high
+    items_bad = Model.search(limit=PAGE_LIMIT, page=10000)
+    assert len(items_bad) == 0
+
+    ids1 = {item.id for item in items1}
+    ids2 = {item.id for item in items2}
+    # assert that items on different pages are all different
+    assert ids1.intersection(ids2) == set()
--- tests/CI/test_node.py
+++ tests/CI/test_node.py
@@ -33,3 +33,20 @@ def test_node_parents(node):
     parents = node.parents
     assert isinstance(parents, list)
     assert all(isinstance(parent, Node) for parent in parents)
+
+
+def test_search_pagination():
+    PAGE_LIMIT = 2
+    items1 = Node.search("power plant", limit=PAGE_LIMIT, page=0)
+    assert len(items1) == PAGE_LIMIT
+    items2 = Node.search("power plant", limit=PAGE_LIMIT, page=1)
+    assert len(items2) == PAGE_LIMIT
+
+    # assert that no items are returned when page number is too high
+    items_bad = Node.search("power plant", limit=PAGE_LIMIT, page=10000)
+    assert len(items_bad) == 0
+
+    ids1 = {item.id for item in items1}
+    ids2 = {item.id for item in items2}
+    # assert that items on different pages are all different
+    assert ids1.intersection(ids2) == set()
