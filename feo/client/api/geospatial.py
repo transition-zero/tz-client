5c0df392b829988bf079153301ca1078b2da4f0a
a-cass <58216420+a-cass@users.noreply.github.com>
2023-11-29 12:21:55 +0000

    feat: change ReadTimeout import

--- feo/client/api/geospatial.py
+++ feo/client/api/geospatial.py
@@ -1,6 +1,6 @@
 from typing import List, Optional, Union
 
-from httpx.exceptions import ReadTimeout
+from httpx import ReadTimeout
 
 from feo.client.api.base import BaseAPI
 from feo.client.api.schemas import FeatureCollection, Geometry
