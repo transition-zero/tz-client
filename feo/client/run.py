b331bfee207f079ee2e38f75c5400abf4acbedbc
Megan Bates <herdofsheep@gmail.com>
2023-11-30 11:51:53 +0000

    building template

--- feo/client/run.py
+++ feo/client/run.py
@@ -15,38 +15,83 @@ class ResultsFilter(schemas.PydanticBaseModel):
     edge_ids: List[str] | str | None = None
 
 
+class Result(schemas.ResultBase):
+    pass
+
+
 class ResultsCollection(pd.DataFrame):
-    _filters: ResultsFilter | None = None
+    """A ResultsCollection is an extention of a Pandas DataFrame.
+
+    It can be used in precisely the same way as a Pandas DataFrame
+    but has a few extra useful constructors.
+
+    Args:
+        _filters (List[Result] | None): TO BE FILLED
+        _scope (schemas.CollectionScope | None): params for generating api query for pagination
+        _page (int | None): if generated from an API query, the current page of the query.
+        _chart_type (str): TO BE FILLED
+        _chart_subtype (str | None): TO BE FILLED
+    """
+
+    _filters: List[Result] | None = None
+    _scope: Optional[schemas.CollectionScope] = None
+    _page: int | None = None
     _chart_type: str
     _chart_subtype: str | None
 
     @property
-    def _constructor(self):
+    def _constructor(self) -> "ResultsCollection":
         return ResultsCollection
 
     @property
-    def _constructor_sliced(self):
+    def _constructor_sliced(self) -> "ResultsCollectionRow":
         return ResultsCollectionRow
+
+    def from_feo_results(cls, results: List[Result]) -> "ResultsCollection":
+        """Instiate an ResultsCollection from a list of results."""
+        # pd.DataFrame.from_records
+        return cls.from_records([r.model_dump() for r in results])
 
     def filter(
         self,
         node_ids: List[str] | str | None = None,
         edge_ids: List[str] | str | None = None,
-    ):
+    ) -> None:
         self._filters = ResultsFilter(node_ids=node_ids, edge_ids=edge_ids)
 
-    def next_page():
-        pass
+    def next_page(self) -> int:
+        """
+        Paginate through results. The Result collection must have a `_scope`.
+        Returns the next page of results and concatenates them in-place to the current collection.
+        """
+        if not self._scope:
+            raise ValueError("Cant iterate an unscoped ResultsCollection")
+        new_collection = self.__class__.from_feo_results(
+            api.results.get(parent_node_id=self._scope.parent_node_id, page=self._page + 1)
+        )
+        self._page += 1
+
+        self.__dict__.update(pd.concat([self, new_collection], ignore_index=True).__dict__)
+        return len(new_collection)
+
+    def to_feo_results(self) -> List[Result]:
+        """Instantiate a list of Results from a ResultsCollection."""
+        return [Result(**row) for idx, row in self.iterrows()]
 
 
 class ResultsCollectionRow(pd.Series):
     @property
-    def _constructor(self):
+    def _constructor(self) -> "ResultsCollectionRow":
         return ResultsCollectionRow
 
     @property
-    def _constructor_expanddim(self):
+    def _constructor_expanddim(self) -> "ResultsCollection":
         return ResultsCollection
+
+    def to_feo_results(self) -> Result:
+        return Result(
+            **self.to_dict(),
+        )
 
 
 class RunResults(schemas.PydanticBaseModel):
@@ -180,12 +225,12 @@ class Run(schemas.RunBase):
             return None
         return factory.scenario(**run_data.scenario.model_dump())
 
-    def __str__(self) -> str:
-        return f"Run: {self.name} (id={self.id})"
-
     @property
-    def results(self):
+    def results(self) -> RunResults:
         if self._run_results is None:
             self._run_results = RunResults(id=self.id)
             return self._run_results
         return self._run_results
+
+    def __str__(self) -> str:
+        return f"Run: {self.name} (id={self.id})"
