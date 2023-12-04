cf8fc078cd5a7ad691a26b1f2848b54392a49023
a-cass <58216420+a-cass@users.noreply.github.com>
2023-12-04 11:14:43 +0000

    feat: WIP: Add Auth class

--- feo/client/api/client.py
+++ feo/client/api/client.py
@@ -3,7 +3,32 @@ import os
 
 import httpx
 
+from feo.client.api.schemas import AuthToken
 from feo.client.auth import login
+
+
+class MissingAuthError(BaseException):
+    pass
+
+
+class Auth:
+    DEFAULT_TOKEN_PATH = os.path.join(os.path.expanduser("~"), ".tz-feo", "token.json")
+    DEFAULT_TOKEN_ENV = "FEO_TOKEN_PATH"  # nosec
+
+    def __init__(self):
+        self.token_path = os.environ.get(self.DEFAULT_TOKEN_ENV, self.DEFAULT_TOKEN_PATH)
+
+        self.token = None
+
+    def authorize(self):
+        login()
+        self.token = AuthToken.from_file(self.token_path)
+
+    def to_header(self):
+        if self.token is None:
+            self.authorize()
+
+        return {"Authorization": f"Bearer {self.token.access_token}"}
 
 
 class Client:
@@ -19,7 +44,10 @@ class Client:
         login()
         token = json.load(open(token_path))
 
-    headers = {"Authorization": "Bearer {}".format(token["access_token"])}
+    headers = {
+        "Authorization": "Bearer {}".format(token["access_token"]),
+        "user-agent": "feo-client",
+    }
 
     base_url = (
         os.environ.get("FEO_API_URL", "https://api.feo.transitionzero.org")
@@ -31,6 +59,12 @@ class Client:
         headers=headers,
         timeout=10,
     )
+
+    def _add_auth(self, func):
+        pass
+
+    def _parse_token(self):
+        pass
 
     def __init__(self):
         pass
