304225297051acc2dfedb69236457f1e8c824a8c
Megan Bates <herdofsheep@gmail.com>
2023-12-14 10:11:18 +0000

    change tests

--- tests/CI/test_asset.py
+++ tests/CI/test_asset.py
@@ -1,4 +1,5 @@
 import pytest
+from httpx import HTTPStatusError
 
 from feo.client import Asset
 
@@ -14,22 +15,27 @@ class TestAsset:
         assert isinstance(items, list)
         assert all(isinstance(asset, Asset) for asset in items)
 
-    @pytest.mark.skip(reason="edge case bug not resolved yet")  # FIXME
     def test_search_pagination(self):
         PAGE_LIMIT = 5
         items1 = Asset.search(alias="Rooppur nuclear power plant", limit=PAGE_LIMIT, page=0)
         assert len(items1) == PAGE_LIMIT
         items2 = Asset.search(alias="Rooppur nuclear power plant", limit=PAGE_LIMIT, page=1)
         assert len(items2) == PAGE_LIMIT
+        items3 = Asset.search(alias="Rooppur nuclear power plant", limit=PAGE_LIMIT, page=2)
+        assert len(items2) == PAGE_LIMIT
 
         # assert that no items are returned when page number is too high
-        items_bad = Asset.search(alias="Rooppur", limit=PAGE_LIMIT, page=10000)
-        assert len(items_bad) == 0
+        with pytest.raises(HTTPStatusError):
+            Asset.search(alias="Rooppur", limit=PAGE_LIMIT, page=10000)
 
         ids1 = {item.id for item in items1}
         ids2 = {item.id for item in items2}
-        # assert that items on different pages are all different
+        ids3 = {item.id for item in items3}
+
+        # check that there is no intersection between the sets by trying to find any matching ids
         assert ids1.intersection(ids2) == set()
+        assert ids2.intersection(ids3) == set()
+        assert ids1.intersection(ids3) == set()
 
     def test_search_with_sector(self):
         items = Asset.search(alias="Rooppur nuclear power plant", sector="power")
