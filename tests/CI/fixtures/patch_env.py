479530039d1b49fc9641c984e134faf7dbfb8220
Lucas Kruitwagen <lucas.kruitwagen@gmail.com>
2024-03-20 11:43:48 +0000

    tests: add env var tests for Client

---
+++ tests/CI/fixtures/patch_env.py
@@ -0,0 +1,11 @@
+import json
+import pytest
+
+@pytest.fixture
+def mock_no_token(monkeypatch):
+    monkeypatch.setenv("TZ_NO_TOKEN", "TRUE")
+
+
+@pytest.fixture
+def mock_some_header(monkeypatch):
+    monkeypatch.setenv("TZ_HEADERS",json.dumps({"x-some-header":"some-value"}))
