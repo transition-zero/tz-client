8a895ebee943f3af79a4b7da7aa65184a881bc0a
silky <noonsilk+-noonsilk@gmail.com>
2024-04-01 09:40:15 +0100

    fix: update model api tests

--- tests/CI/test_model_api.py
+++ tests/CI/test_model_api.py
@@ -1,18 +1,14 @@
-import pytest
-
 from tz.client import api
 
-EXAMPLE_MODEL = "feo-global-indonesia"
+EXAMPLE_MODEL = "feo-indonesia"
 
 
-@pytest.mark.xfail(reason="v2 migration wip")
 def test_model_search():
     models = api.models.search()
     assert isinstance(models, list)
-    assert isinstance(models[0], api.schemas.Model)
+    assert isinstance(models[0], api.generated_schema.Model)
 
 
-@pytest.mark.xfail(reason="v2 migration wip")
 def test_model_get():
-    models = api.models.get(EXAMPLE_MODEL)
-    assert isinstance(models, api.schemas.Model)
+    models = api.models.get(owner="me", model_slug=EXAMPLE_MODEL)
+    assert isinstance(models, api.generated_schema.Model)
